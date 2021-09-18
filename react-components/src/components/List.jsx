ebd8822c5fce9775158479250855791faed99693
Deepak Verma <0822deepakverma@gmail.com>
2021-09-18 11:00:07 +0530

    React-component

---
+++ react-components/src/components/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>Angel & Demons</li>
+      <li>Three man in a Boat</li>
+      <li>Story of a young girl</li>
+    </ul>
+  );
+}
+
+export default List;
