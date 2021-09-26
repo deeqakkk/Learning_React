ee3cbf6758dc8e71ad72620c43c9028d7d2266a4
Deepak Verma <0822deepakverma@gmail.com>
2021-09-27 00:42:06 +0530

    mapping-components

---
+++ mapping-components-practice/src/emojipedia.js
@@ -0,0 +1,45 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "🐦",
+    name: "Bird",
+    meaning:
+      "Tell me about the birds and the bees. Flip someone the bird or to be a bird brain. Bird emoticon stands for freedom and fantasy. Can also symbolize the soul on a spiritual level"
+  },
+  {
+    id: 5,
+    emoji: "🐶",
+    name: "Dog Face",
+    meaning:
+      "A sweet dog face with a panting tongue and floppy ears. Stands for unconditional loyalty and attention. Can refer to your own pet (“I love my dog.“) or to people (“You sly dog!“)."
+  },
+  {
+    id: 6,
+    emoji: "🙈",
+    name: "See-No-Evil Monkey",
+    meaning:
+      "Monkey with two hands in front of its eyes. Closes its eyes to the world so as not to see anything bad or cruel, or is embarrassed. Mizaru is one of the three wise monkeys. In a sense, ignoring bad things."
+  }
+];
+export default emojipedia;
