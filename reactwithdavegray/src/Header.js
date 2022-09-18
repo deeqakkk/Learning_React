a08fed63ca8866a7c3da27d18169588f8cf12d0b
Deepak <0822deepakverma@gmail.com>
2022-09-18 10:55:13 +0530

    Props, components, prop drilling added

---
+++ reactwithdavegray/src/Header.js
@@ -0,0 +1,11 @@
+function Header(props) {
+  return (
+    <header>
+      <h1> {props.title} </h1>
+    </header>
+  );
+}
+Header.defaultProps = {
+  title: "Default List"
+};
+export default Header;
